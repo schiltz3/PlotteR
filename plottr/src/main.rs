78b3749ef6e6461840c3271e2ab4be10532fedfd
John Schiltz <johnschiltz2000@gmail.com>
2022-01-16 02:05:34 -0600

    add plottr

---
+++ plottr/src/main.rs
@@ -0,0 +1,63 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
+use std::{env, vec};
+
+use anyhow::{anyhow, Result};
+
+#[derive(Debug)]
+struct Column {
+    header: String,
+    column: Vec<f64>,
+}
+
+fn run() -> Result<()> {
+    parse_columns()?;
+    Ok(())
+}
+
+fn parse_columns() -> Result<Vec<Column>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(file);
+
+    let mut columns: Vec<Column> = rdr
+        .headers()?
+        .into_iter()
+        .map(|header| Column {
+            header: header.into(),
+            column: vec![],
+        })
+        .collect();
+
+    for row in rdr.records() {
+        for (column, raw_column) in columns.iter_mut().zip(row?.into_iter()) {
+            column.column.push(raw_column.parse()?);
+        }
+    }
+    Ok(columns)
+}
+
+fn get_first_arg() -> Result<OsString> {
+    match env::args_os().nth(1) {
+        None => Err(anyhow!("expected 1 argument, but got none:")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+    let app = plottr::TemplateApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
+}
