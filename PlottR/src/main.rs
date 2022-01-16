fc613154df6f27e1e8f8621e28fb8a4cbf1eb8fe
John Schiltz <johnschiltz2000@gmail.com>
2022-01-16 01:32:17 -0600

    Deserialize csv

--- PlottR/src/main.rs
+++ PlottR/src/main.rs
@@ -1,3 +1,57 @@
+extern crate csv;
+
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
+use std::{env, vec};
+
+use anyhow::{anyhow, Result};
+
+#[derive(Debug)]
+struct Column {
+    header: String,
+    column: Vec<f64>,
+}
+fn run() -> Result<()> {
+    parse_columns()?;
+    Ok(())
+}
+
+
+fn parse_columns() -> Result<Vec<Column>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(file);
+
+    let mut columns: Vec<Column> = rdr
+        .headers()?
+        .into_iter()
+        .map(|header| Column {
+            header: header.into(),
+            column: vec![],
+        })
+        .collect();
+
+    for row in rdr.records() {
+        for (column, raw_column) in columns.iter_mut().zip(row?.into_iter()) {
+            column.column.push(raw_column.parse()?);
+        }
+    }
+    Ok(columns)
+}
+
+fn get_first_arg() -> Result<OsString> {
+    match env::args_os().nth(1) {
+        None => Err(anyhow!("expected 1 argument, but got none:")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
--- PlottR/target/debug/deps/PlottR-d3e9ebbe0aa91b16.d
+++
@@ -1,5 +0,0 @@
-e:\git\PlotteR\PlottR\target\debug\deps\PlottR-d3e9ebbe0aa91b16.rmeta: src\main.rs
-
-e:\git\PlotteR\PlottR\target\debug\deps\PlottR-d3e9ebbe0aa91b16.d: src\main.rs
-
-src\main.rs:
